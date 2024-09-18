988350cead28382dfcdf17acaceba64cef2b4555
Hojniak Dominika <skubik1411@gmail.com>
2024-09-18 21:37:57 +0200

    first commit

---
+++ backend/src/main/java/com/justory/backend/JustoryApplication.java
@@ -0,0 +1,13 @@
+package com.justory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustoryApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/justory/backend/JustoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justory.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
